8eb9ab8fefe9752291d7f5b2276607e1374c6bd7
hallshit <nigel672@gmail.com>
2017-07-14 22:57:42 -0700

    first commit

---
+++ tradierAPI.py
@@ -0,0 +1,29 @@
+import requests
+import urllib2
+import json
+
+baseurl = 'https://sandbox.tradier.com/v1/markets/quotes?symbols=SPY,TSLA,EEM'
+accessToken = 'sw5BqGTUxcV8Gm5V0umAX8Mfvcfd'
+
+
+
+
+def main():
+    req = urllib2.Request(baseurl)
+    req.add_header('Authorization', 'Bearer {}'.format(accessToken))
+    req.add_header('Accept', 'application/json')
+    f = urllib2.urlopen(req)
+    quotes = f
+    quotes = json.load(quotes)
+    quotes = quotes['quotes']['quote']
+    tickerstring = ''
+    for x in quotes:
+        singleString = x['symbol'] + " " + str(x['last']) + " " + str(x['change_percentage'])
+        tickerstring += " " + singleString
+    print tickerstring
+
+
+
+
+if __name__ == '__main__':
+    main()
